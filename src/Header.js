e32488d37959856fac2ab12dac963ad256dbfc70
darkmal <vagnerwillians@hotmail.com>
2022-11-08 22:18:59 -0300

    08-11 22:18HS

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header>
+            <div className="firstMessage">I will make you Front Developer</div>
+            <div>Mein Logo 4</div>
+        </header>
+    )
+}
+
+export default Header;
