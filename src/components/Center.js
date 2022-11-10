b106edba82dbf9fe4b3684adb572977602f4ed10
darkmal <vagnerwillians@hotmail.com>
2022-11-10 16:35:52 -0300

    10-11 16:35hs

--- src/components/Center.js
+++ src/components/Center.js
@@ -5,7 +5,7 @@ const Center = () => {
     return (
         <div className="center">
             <div className="form-container">
-                <h2>Enter your name and email to get the book</h2>
+                <h2>Enter your name and email<br/> to get the book</h2>
                 <form>
                     <input type='text' name='name' placeholder="Name"></input>
                     <input type='text' name='email' placeholder='Email'></input>
