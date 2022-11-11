2bb92568e327162d04cce75dce41bf66c9cb1243
darkmal <vagnerwillians@hotmail.com>
2022-11-11 19:58:10 -0300

    11-11

--- src/components/Center.js
+++ src/components/Center.js
@@ -4,6 +4,12 @@ import "./Center.css";
 const Center = () => {
     return (
         <div className="center">
+            <div className="myText">
+                <h1>The complete guide that makes you a Fron-end Developer</h1>
+                <p>Find out the necessary technologies to land your first job - you don't need to know everthing!</p>
+                <p>You will learn to create your own page with portifolio to show clients and businesses.</p>
+                <p>You will master the combination of html, css and javascript to more productive in less time.</p>
+             </div>
             <div className="form-container">
                 <h2>Enter your name and email<br/> to get the book</h2>
                 <form>
