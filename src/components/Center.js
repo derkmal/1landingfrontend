eb518ffa8ba36011891fdacf837deb3b5d5cd141
darkmal <vagnerwillians@hotmail.com>
2022-11-12 16:19:03 -0300

    12-11 16:18hs

--- src/components/Center.js
+++ src/components/Center.js
@@ -7,9 +7,10 @@ const Center = () => {
         <div className="center">
             <div className="myText">
                 <h1>The complete guide that makes you a Front-end Developer</h1>
-                <p>Find out the necessary technologies to land your first job - you don't need to know everthing!</p>
-                <p>You will learn to create your own portifolio page to showcase you work to clients and businesses.</p>
-                <p>You will master the combination of html, css and javascript to be more productive in less time.</p>
+                <p>This course introduces you the basic technologies to land your first job - you don't need to know everthing!</p>
+                <p>You will learn to create your own portifolio to showcase your work to clients and businesses.</p>
+                <p>You will master html, css and javascript to be more productive in less time.</p>
+                <p>As you will progress in the course you will be able to produce results in real-time. </p>
             </div>
 
             <div className="form-container">
@@ -17,11 +18,11 @@ const Center = () => {
                 <form>
                     <input type='text' name='name' placeholder="Name"></input>
                     <input type='text' name='email' placeholder='Email'></input>
-                    <input type='submit' value='OK. I Want the Course!'></input>
+                    <input type='submit' value='I Want to be a Web Developer!'></input>
                 </form>            
             </div>
 
-            <div className="clear"></div>
+            {/* <div className="clear"></div> */}
         </div>
     )
 }
