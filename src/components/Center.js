c368ad2d445b4de68be37455fc460041ae8a5b47
darkmal <vagnerwillians@hotmail.com>
2022-11-12 11:22:41 -0300

    12-11

--- src/components/Center.js
+++ src/components/Center.js
@@ -1,14 +1,15 @@
 import React from "react";
 import "./Center.css";
 
+
 const Center = () => {
     return (
         <div className="center">
             <div className="myText">
                 <h1>The complete guide that makes you a Fron-end Developer</h1>
                 <p>Find out the necessary technologies to land your first job - you don't need to know everthing!</p>
-                <p>You will learn to create your own page with portifolio to show clients and businesses.</p>
-                <p>You will master the combination of html, css and javascript to more productive in less time.</p>
+                <p>You will learn to create your own portifolio page to showcase you work to clients and businesses.</p>
+                <p>You will master the combination of html, css and javascript to be more productive in less time.</p>
              </div>
             <div className="form-container">
                 <h2>Enter your name and email<br/> to get the book</h2>
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,11 @@ import React from "react";
 import Image from "../images/Símbolo-Estilizado-BONUS.jpg";
 import "./Header.css";
 
+
 const Header = () => {
     return (
         <header>
-            <div className="firstMessage"><p>The truth<br/> about being a Front-end Developer<span></span></p></div>
+            <div className="firstMessage"><p>The truth<br/> <span>about being a Front-end Developer</span></p></div>
             <div className="myImage"><img src={Image} alt="logo"/></div>
             {/* <div className="clear"></div> */}
         </header>
