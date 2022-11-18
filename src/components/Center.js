eec9576379ed329044067a32133b119e62982922
darkmal <vagnerwillians@hotmail.com>
2022-11-17 23:53:40 -0300

    17-11 23:53hs

--- src/components/Center.js
+++ src/components/Center.js
@@ -7,13 +7,13 @@ const Center = () => {
         <div className="center">
             <div className="myText">
                 <h1>THE COMPLETE FRONT-END DEVELOPER GUIDE<br/>A PATH FROM BEGINNER TO PROFESSINAL</h1>
-                <ul>
-                    <li>Know the basic technologies to land your first job - you don't need to know everthing!</li>
-                    <li>We'll create your own portifolio to showcase your work to clients and businesses.</li>
-                    <li>You will master html, css and javascript to be more productive in less time.</li>
-                    <li>As you will progress in the course you will be able to produce tangible results in real-time. </li>
-                    <li>If you alread have the basics, no problem, we can develop your skills to be proficient.</li>
-                </ul>
+                {/* <ul> */}
+                    <p>Know the basic technologies to land your first job - you don't need to know everthing!</p>
+                    <p>We'll create your own portifolio to showcase your work to clients and businesses.</p>
+                    <p>You will master html, css and javascript to be more productive in less time.</p>
+                    <p>As you will progress in the course you will be able to produce tangible results in real-time. </p>
+                    <p>If you alread have the basics, no problem, we can develop your skills to be proficient.</p>
+                {/* </ul> */}
             </div>
 
             <div className="form-container">
@@ -22,14 +22,14 @@ const Center = () => {
                     <input type='text' name='name' placeholder="Name"></input>
                     <input type='text' name='email' placeholder='Email'></input>
                     <input type='submit' value='I Want to be a Web Developer!'></input>
-                </form>            
+                </form>  
+                <div>fdsfadsfas</div>          
             </div>
 
-            <div className="clear"></div>
+            {/* <div className="clear"></div> */}
         </div>
     )
 }
-
 
 export default Center;
 
